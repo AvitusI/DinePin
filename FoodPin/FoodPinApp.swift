08315781eb4994fbd7be9f4d6a48653227ae7dfe
AvitusI <avitussweetbert213@gmail.com>
2025-03-22 16:55:42 +0300

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by MacMini on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
